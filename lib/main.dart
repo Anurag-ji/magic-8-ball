56a445c2ec11f95805b37993be6a97c8237205de
Anurag-ji <trivedianurag619@gmail.com>
2021-10-16 14:35:02 +0530

    adding files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Ballpage(),
       ),
     );
+
+class Ballpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballface = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                ballface = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballface.png'),
+          ),
+        ),
+      ],
+    );
+  }
+}
